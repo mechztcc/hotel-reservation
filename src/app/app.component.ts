0715c89c7a8a978a77fd584dbe9b697fda78ae72
mechztcc <dojathan@gmail.com>
2023-01-17 09:39:12 -0300

    feat: adicionado fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faCoffee, faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'hotel-calendar';
+  faCoffee = faCoffee;
+  faCog = faCog;
 }
